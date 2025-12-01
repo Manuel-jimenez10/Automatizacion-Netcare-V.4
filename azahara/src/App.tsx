3c2aac20736d1ac9ec4353c51f9e4a78d8f8a0d5
Edwar Jimenez <Jimenzeddi@gmail.com>
2025-12-01 18:27:13 -0400

    configuracion tailwind y routing

--- azahara/src/App.tsx
+++ azahara/src/App.tsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router'
 import './App.css'
+import { Navbar } from './components/navBar'
+import Home from './components/Home'
 
 function App() {
 
   return (
     <>
-      
+    <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
     </>
   )
 }
--- azahara/src/components/navBar.tsx
+++ azahara/src/components/navBar.tsx
@@ -1,3 +1,12 @@
+import { useNavigate } from "react-router"
+
 export const Navbar = () => { 
-    
+      const navigate = useNavigate();
+
+    return (
+        <nav>
+            <ul className="">
+                <li className="hover:text-red-500 font-bold" onClick={()=>navigate('/home')}>home</li>
+                </ul> </nav>
+    )
  }
